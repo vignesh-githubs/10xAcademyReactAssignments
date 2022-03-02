658e04e3c1b17bbff7a5f82400dc2d87b3a1bf2f
vigneshwar <vigneshwargoud29599@gmail.com>
2022-03-03 04:03:50 +0530

    comitting Assignment2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -9,9 +9,28 @@ function App(props) {
 return(
   <div className="container">
   {/* Start your React code here */}
+  <div className="employee profile">
+  <img className="avtar "src={employee.profileImg} alt="avtrar"/>
+
+  <h1 className="name">{employee.name}</h1>
+
+ 
+  <div className='content'><label className="label">Location</label><br></br><bold className='data'>{employee.location}</bold></div>
+
+  <div className='content'><label className="label">Blood group</label><br></br><bold className='data'>{employee.bloodGroup}</bold></div>
+
+
+  <div className='content'><label className="label">Age</label><br></br><bold className='data'>{employee.age}</bold></div>
+
+
+  </div>
+  
+  
+  
 </div>
 )
 }
+
 
 
 
